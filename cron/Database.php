d95d39b5f417b75c8f6ba781c89eafc12557b440
Maxime De Braekeleer <maxime.debraekeleer.y7196@student.hogent.be>
2019-03-19 10:29:38 +0100

    Init commit

---
+++ cron/Database.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "51.15.88.73";
+$username = "maxime";
+$password = "DeBraekeleer6";
+$dbname = 'grepolistools';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
