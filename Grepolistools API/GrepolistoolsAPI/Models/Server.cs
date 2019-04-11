aa9bdc038efc007a29d8874da2874b83289b2e77
Maxime De Braekeleer <maxime.debraekeleer.y7196@student.hogent.be>
2019-04-11 22:58:40 +0200

    Start Models

---
+++ Grepolistools API/GrepolistoolsAPI/Models/Server.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrepolistoolsAPI.Models
+{
+    public class Server
+    {
+        #region props
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<World> Worlds { get; private set; }
+
+        #endregion
+
+        #region ctor 
+
+        public Server(string Name)
+        {
+            this.Name = Name;
+        }
+
+        #endregion
+
+        #region methods
+
+        public void AddWorld(World world) => Worlds.Add(world);
+
+        public World GetWorld(int id) => Worlds.SingleOrDefault(w => w.Id == id);
+
+        #endregion
+    }
+}
---
+++ Grepolistools API/GrepolistoolsAPI/Models/World.cs
@@ -0,0 +1,13 @@
+﻿namespace GrepolistoolsAPI.Models
+{
+    public class World
+    {
+        #region props
+
+        public int Id { get; set; }
+        public string Server { get; private set; }
+        public string Name { get; set; }
+
+        #endregion
+    }
+}
