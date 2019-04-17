f4eb538d19a7ecf70bf32dd5e230a6d1474ae214
Maxime De Braekeleer <maxime.debraekeleer.y7196@student.hogent.be>
2019-04-17 12:37:17 +0200

    Prop removed from World

--- GrepolistoolsAPI/GrepolistoolsAPI/Data/Mappers/WorldConfiguration.cs
+++ GrepolistoolsAPI/GrepolistoolsAPI/Data/Mappers/WorldConfiguration.cs
@@ -22,30 +22,6 @@ namespace GrepolistoolsAPI.Data.Mappers
             builder.HasMany(w => w.towns).WithOne(t => t.World).HasForeignKey(t => new { t.World_Id, t.Server_Name });
             builder.HasMany(w => w.conquers).WithOne(c => c.World).HasForeignKey(c => new { c.World_Id, c.Server_Name });
 
-            //builder.OwnsMany(w => w.players, w => {
-
-            //    w.HasForeignKey(p => new { p.World_Id, p.Server_Name });
-            //    w.HasKey(p => new { p.Id, p.World_Id, p.Server_Name, p.Date });
-
-            //});
-            //builder.OwnsMany(w => w.towns, w => {
-
-            //    w.HasForeignKey(t => new { t.World_Id, t.Server_Name });
-            //    w.HasKey(t => new { t.Id, t.World_Id, t.Server_Name, t.Date });
-
-            //});
-            //builder.OwnsMany(w => w.alliances, w => {
-
-            //    w.HasForeignKey(a => new { a.World_Id, a.Server_Name });
-            //    w.HasKey(a => new { a.Id, a.World_Id, a.Server_Name, a.Date });
-
-            //});
-            //builder.OwnsMany(w => w.conquers, w => {
-
-            //    w.HasForeignKey(c => new { c.World_Id, c.Server_Name });
-            //    w.HasKey(c => new { c.Date, c.Town_Id, c.World_Id, c.Server_Name });
-
-            //});
             builder.HasOne(w => w.Server).WithMany(s => s.Worlds).HasForeignKey(w => w.Server_Name).OnDelete(DeleteBehavior.Restrict);
         }
     }
--- GrepolistoolsAPI/GrepolistoolsAPI/Models/World.cs
+++ GrepolistoolsAPI/GrepolistoolsAPI/Models/World.cs
@@ -7,18 +7,14 @@ namespace GrepolistoolsAPI.Models
     public class World
     {
         #region props
-        [Required]
         public int Id { get; private set; }
-        [Required]
         public Server Server { get; private set; }
         public string Server_Name { get; private set; }
-        [Required]
         public string Name { get; private set; }
         public bool isOpen { get; set; }
         [Required]
         public bool isDomination { get; set; }
         public DateTime lastModified { get; set; }
-        public DateTime? closingDate { get; set; }
 
         public ICollection<Town> towns { get; private set; }
         public ICollection<Player> players { get; private set; }
