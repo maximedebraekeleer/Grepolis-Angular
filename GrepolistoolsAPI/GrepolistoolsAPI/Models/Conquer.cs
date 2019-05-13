71efaf9eaca8ffc037cc423890f47410870c9344
Maxime De Braekeleer <maxime.debraekeleer.y7196@student.hogent.be>
2019-05-13 16:51:46 +0200

    test commit

--- GrepolistoolsAPI/GrepolistoolsAPI/Models/Conquer.cs
+++ GrepolistoolsAPI/GrepolistoolsAPI/Models/Conquer.cs
@@ -9,10 +9,10 @@ namespace GrepolistoolsAPI.Models
         public World World { get; set; }
         public int World_Id { get; private set; }
         public string Server_Name { get; private set; }
-        public int OldOwner { get; private set; }
+        public int? OldOwner { get; private set; }
         public int NewOwner { get; private set; }
-        public int OldAlliance { get; private set; }
-        public int NewAlliance { get; private set; }
+        public int? OldAlliance { get; private set; }
+        public int? NewAlliance { get; private set; }
         public int Points { get; private set; }
     }
 }
