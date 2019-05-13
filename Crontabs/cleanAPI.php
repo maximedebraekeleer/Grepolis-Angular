113d0dd3b42577516a29a19048e46eecfaec3814
Maxime De Braekeleer <maxime.debraekeleer.y7196@student.hogent.be>
2019-05-13 09:50:29 +0200

    Kleine aanpassing cleanAPI script.

--- Crontabs/cleanAPI.php
+++ Crontabs/cleanAPI.php
@@ -1,11 +1,31 @@
 <?php
 
 require_once("Database.php");
-$OneMonth = date("Y-m-d", strtotime("-1 Month"));
+$twoWeeks = date("Y-m-d", strtotime("-2 Week"));
 $OneWeek = date("Y-m-d", strtotime("-1 Week"));
 
-sqlsrv_query($conn, "DELETE FROM Town WHERE Date < '$OneWeek' ");
-sqlsrv_query($conn, "DELETE FROM Alliance WHERE Date < '$OneMonth' ");
-sqlsrv_query($conn, "DELETE FROM Player WHERE Date < '$OneMonth' ");
+
+
+$getWorlds = sqlsrv_query($conn, "SELECT Id, Server_Name, isOpen FROM World");
+
+while($world = sqlsrv_fetch_array($getWorlds, SQLSRV_FETCH_ASSOC))
+{
+    if($world['isOpen'])
+    {
+        sqlsrv_query($conn, "DELETE FROM Town WHERE Date < '$OneWeek' AND Server_Name = '$world[Server_Name]' AND World_Id = '$world[Id]'");
+        sqlsrv_query($conn, "DELETE FROM Alliance WHERE Date < '$twoWeeks' AND Server_Name = '$world[Server_Name]' AND World_Id = '$world[Id]' ");
+        sqlsrv_query($conn, "DELETE FROM Player WHERE Date < '$twoWeeks' AND Server_Name = '$world[Server_Name]' AND World_Id = '$world[Id]' ");
+    }else
+    {
+        $dates = sqlsrv_query($conn, "SELECT DISTINCT(Date) FROM Player WHERE Server_Name = '$world[Server_Name]' AND World_Id = '$world[Id]' Order By Date Desc");
+        $date = sqlsrv_fetch_array($dates, SQLSRV_FETCH_NUMERIC);
+        $lastDate = date("Y-m-d", strtotime($date[0]->format("Y-m-d")));
+
+        sqlsrv_query($conn, "DELETE FROM Town WHERE Date < '$lastDate' AND Server_Name = '$world[Server_Name]' AND World_Id = '$world[Id]'");
+        sqlsrv_query($conn, "DELETE FROM Alliance WHERE Date < '$lastDate' AND Server_Name = '$world[Server_Name]' AND World_Id = '$world[Id]' ");
+        sqlsrv_query($conn, "DELETE FROM Player WHERE Date < '$lastDate' AND Server_Name = '$world[Server_Name]' AND World_Id = '$world[Id]' ");
+ 
+    }
+}
 
 ?>
